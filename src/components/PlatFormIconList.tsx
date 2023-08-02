915349958f2ae6282f075eca6cea86765a5bb595
Akshay Raju <akshayvg4195@gmail.com>
2023-08-02 11:12:09 -0400

    Missed a file

--- src/components/PlatFormIconList.tsx
+++ src/components/PlatFormIconList.tsx
@@ -1,16 +1,39 @@
 import { Platform } from "../hooks/useGames";
-import { Text } from "@chakra-ui/react";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
 const PlatFormIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    nintendo: SiNintendo,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    web: BsGlobe,
+  };
   return (
-    <>
+    <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
       ))}
-    </>
+    </HStack>
   );
 };
 
