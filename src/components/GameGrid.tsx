374910a2daf63f6de97cc4282680fc5f1a0fa4e7
Akshay Raju <akshayvg4195@gmail.com>
2023-08-03 13:40:08 -0400

    Fixed the issue with chakra menus

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,6 @@ const GameGrid = ({ gameQuery }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
